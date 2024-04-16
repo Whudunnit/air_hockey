d60c39ccef95cb32af6aea54fec510b0004cff71
Regéczy Zsombor <45742361+Whudunnit@users.noreply.github.com>
2024-04-16 13:00:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const canvas = document.createElement('canvas');
-const w = canvas.width = 700;
-const h = canvas.height = 900;
-document.body.appendChild(canvas);
+const canvas = document.querySelector('canvas');
+const w = canvas.width = 500;
+const h = canvas.height = 700;
 const context = canvas.getContext('2d');
 const scoreElement = document.getElementById('score');
 const ComputerScoreElement = document.getElementById('computer_score');
 
-const board = () => {
 
-  
-    context.beginPath();
-    context.arc(w/2,h/2,w/10,0,2*Math.PI);
-    context.moveTo(0,h/2);
-    context.lineTo(w,h/2);
-    context.stroke();
-    
+const backgroundImage = new Image();
+backgroundImage.src = 'hockey_board.png';
+
+backgroundImage.onload = function() {
+    // Draw the background image
+    context.drawImage(backgroundImage, 0, 0, w, h);
 }
 
 window.addEventListener('mousemove', (e) =>{
@@ -30,13 +27,12 @@ class Player {
         this.prevY = undefined;
         this.dx = undefined;
         this.dy = undefined;
-        this.maxY = h / 2;
     }
 
     draw (){
         context.beginPath();
         context.arc(this.x, this.y, w*0.05, 0, 2*Math.PI);
-        context.fillStyle = "red";
+        context.fillStyle = "blue";
         context.fill();
         context.stroke();
     }
@@ -47,9 +43,10 @@ class Player {
         this.prevX = this.x;
         this.prevY = this.y;
         this.x = Math.max(Math.min(this.x, w - w*0.05), w*0.05);
-        this.y = Math.max(Math.min(this.y, this.maxY - w * 0.05), 0);       
+        this.y = Math.max(Math.min(this.y, h - w * 0.05), w*0.05);       
     }
 }
+
 let playerScore = 0;
 let computerScore = 0;
 class Puck {
@@ -79,30 +76,41 @@ class Puck {
             this.dx *=-1;
         }
 
-        if(this.y - w*0.04 <= 0){
-            // Increment player's score
-            playerScore++;
-            // Update the score element text content
-            scoreElement.textContent = 'Player score: ' + playerScore;
-            // Reset puck position to the center
-            this.x = w/2;
-            this.y = h/2;
-            this.dx = 0;
-            this.dy = 0;
-            redrawBoard(player, computer);
+        if(this.y + w*0.04 >= h || this.y - w*0.04 <= 0){
+            this.dy *=-1;
         }
 
-        if(this.y + w*0.04 >= h){
-            // Increment player's score
-            computerScore++;
-            // Update the score element text content
-            ComputerScoreElement.textContent = 'Computer score: ' + computerScore;
-            // Reset puck position to the center
-            this.x = w/2;
-            this.y = h/2;
-            this.dx = 0;
-            this.dy = 0;
-            redrawBoard(player, computer);
+        if(this.y - w*0.04 < 0){
+            if(this.x > w/3 && this.x < 2*w/3){
+                playerScore++;
+                if(playerScore == 11){
+                    window.alert("You Won!");
+                    playerScore = 0;
+                    computerScore = 0;
+                }
+                scoreElement.textContent = 'Player score: ' + playerScore;
+                this.x = w/2;
+                this.y = h/2;
+                this.dx = 0;
+                this.dy = 0;
+                redrawBoard(player, computer);
+            }
+        }
+        else if(this.y + w*0.04 > h){
+            if(this.x >w/3 && this.x < 2*w/3){
+                computerScore++;
+                if(computerScore == 11){
+                    window.alert("The Computer Won!");
+                    playerScore = 0;
+                    computerScore = 0;
+                }
+                ComputerScoreElement.textContent = 'Computer score: ' + computerScore;
+                this.x = w/2;
+                this.y = h/2;
+                this.dx = 0;
+                this.dy = 0;
+                redrawBoard(player, computer);
+            }
         }
 
         // Collision
@@ -152,15 +160,15 @@ class Computer {
     constructor(){
         this.x = w/2;
         this.y = h/10;
-        this.dy = 0;
-        this.dy = 0;
+        this.dy = 3;
+        this.dy = 3;
         this.defaultPosition = { x: this.x, y: this.y };
     }
 
     draw(){
         context.beginPath();
         context.arc(this.x, this.y, w*0.05, 0, 2*Math.PI);
-        context.fillStyle = "blue";
+        context.fillStyle = "red";
         context.fill();
         context.stroke();
     }
@@ -176,15 +184,15 @@ class Computer {
     }
 
     strike(){
-        const dx = puck.x - this.x-0.01;
-        const dy = puck.y - this.y-0.01;
-        const angle = Math.atan2(dy, dx)-0.01;
+        const dx = puck.x - this.x;
+        const dy = puck.y - this.y;
+        const angle = Math.atan(dy, dx);
 
 
-        const speed = 25;
+        const speed = 20;
 
-        this.x += Math.cos(angle) * speed-0.01;
-        this.y += Math.sin(angle) * speed-0.01;
+        this.x += Math.cos(angle) * speed;
+        this.y += Math.sin(angle) * speed;
     }
 
     return(){
@@ -196,11 +204,19 @@ class Computer {
 }
 
 function redrawBoard() {
-    context.clearRect(0, 0, w, h);
-    board();
     player.draw();
     computer.draw();
 }
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetBoard);
+
+function resetBoard(){
+    puck.x = w / 2;
+    puck.y = h / 2;
+    puck.dx = 0;
+    puck.dy = 0;
+};
 
 const player = new Player;
 const puck = new Puck;
@@ -208,13 +224,13 @@ const computer = new Computer;
 
 function animate(){
 
-    redrawBoard();
+    context.drawImage(backgroundImage, 0, 0, w, h);
     puck.update();
     player.update();
     computer.update();
-    puck.draw();
     player.draw();
     computer.draw();
+    puck.draw();
     requestAnimationFrame(animate);
 }
 animate();
