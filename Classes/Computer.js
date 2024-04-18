960f2b3b6f0ba212fc2e074c73ee322711ff69db
Regéczy Zsombor <45742361+Whudunnit@users.noreply.github.com>
2024-04-18 20:36:16 +0200

    Add files via upload

--- Classes/Computer.js
+++ Classes/Computer.js
@@ -2,10 +2,9 @@ class Computer {
     constructor(){
         this.x = w/2;
         this.y = h/10;
-        this.dy = 3;
+        this.dx = 3;
         this.dy = 3;
         this.defaultPosition = { x: this.x, y: this.y };
-        this.rectangle = { minX: 0, maxX: 500, minY: 350, maxY: 700 };
     }
 
     draw(){
@@ -17,22 +16,28 @@ class Computer {
     }
 
     update(){
-        if(Math.sign(puck.dy) === 1) {
+        if (Math.sign(puck.dy) === 1) {
             this.return();
-        }else if (puck.y < this.y){
+        } else if (puck.y < this.y){
             this.return();
-        }else if (Math.sign(puck.dy) === -1 && puck.y < h/2){
+        } else if (Math.sign(puck.dy) === -1 && puck.y < h/2){
             this.strike();
         }
+        const distance = Math.sqrt(Math.pow((puck.x - this.x), 2) + Math.pow((puck.y - this.y), 2));
+        if (distance < 50 || (Math.sign(puck.dy) === -1 && puck.y < h/2)) {
+            this.strike();
+        } else {
+            this.return();
+        }
+
     }
 
     strike(){
         const dx = puck.x - this.x;
         const dy = puck.y - this.y;
-        const angle = Math.atan(dy, dx);
+        const angle = Math.atan2(dy, dx);
 
-
-        const speed = 15;
+        const speed = 3;
 
         this.x += Math.cos(angle) * speed;
         this.y += Math.sin(angle) * speed;
@@ -41,7 +46,7 @@ class Computer {
     return(){
         this.dx = 3;
         this.dy = 3;
-        this.x += this.x > this.defaultPosition.x ? this.dx*-1 :this.dx
-        this.y += this.y > this.defaultPosition.y ? this.dy*-1 :this.dy
+        this.x += this.x > this.defaultPosition.x ? this.dx*-1 : this.dx;
+        this.y += this.y > this.defaultPosition.y ? this.dy*-1 : this.dy;
     }
 }
